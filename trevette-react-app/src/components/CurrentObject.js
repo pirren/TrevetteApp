aa46bf36e999c38c17746351cc56a00089d4d7f2
Pierre <pierre.nygard@gmail.com>
2020-10-01 23:55:54 +0200

    framsteg på design

--- trevette-react-app/src/components/CurrentObject.js
+++ trevette-react-app/src/components/CurrentObject.js
@@ -19,7 +19,6 @@ export default class CurrentObject extends Component {
                         <p>Den nya porschen är riktigt fet. Har en 3 L motor och gör 100 km/h på 3.7 s med sport-chrono paket. Denna vagn är för dom riktigt rika. </p>
                         <div className="sale">
                             <button type="button" className="btn btn-lg btn-block">Förfråga om pris</button>
-                            <h5 class="price">Uppskattat värde: 110000:-</h5>
                         </div>
                     </div>
                     <ObjectGallery />
