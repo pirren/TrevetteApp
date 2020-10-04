82a3ac4ca6ddefb7e0b4ff14f7965bd2735d4921
Pierre <pierre.nygard@gmail.com>
2020-10-04 22:03:53 +0200

    alla resources returnerar länkade collection och objekt som resources

--- trevette-api/Resources/SalesObjectResource.cs
+++ trevette-api/Resources/SalesObjectResource.cs
@@ -9,7 +9,7 @@ namespace trevette_api.Resources
     public class SalesObjectResource
     {
         public int SalesObjectId { get; set; }
-        public Car Car { get; set; }
+        public CarResource Car { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int? Price { get; set; }
